a02c0be2634e5482ae19b1fdb10bbf387aaa2375
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2025-09-13 14:49:49 +0100

    feat: add sign in url to clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/admin(.*)"])
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect({ unauthenticatedUrl: "/auth/sign-in" })
-  }
-})
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (isProtectedRoute(req)) {
+      await auth.protect()
+    }
+  },
+  { signInUrl: "/auth/sign-in" }
+)
 
 export const config = {
   matcher: [
